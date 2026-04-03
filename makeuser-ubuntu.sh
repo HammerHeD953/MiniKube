d50fcbffd44a6b6602b86a9abc7e8d36d82d9c8e
HammerHeD953 <HammerHeD953@gmail.com>
2026-04-03 20:43:48 +0300

    Delete_secret_key

--- makeuser-ubuntu.sh
+++ makeuser-ubuntu.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 username=ansible
-sshkey="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEmegYSWcme2WLRF4t2ZFCq+bzoM+q0Ed6LpKsRdQrS7 games@Gamer-PC"
-id_ed25519=""
-id_ed25519_pub="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEmegYSWcme2WLRF4t2ZFCq+bzoM+q0Ed6LpKsRdQrS7 games@Gamer-PC"
+sshkey="pub"
+id_ed25519="secret"
+id_ed25519_pub="pub"
 useradd -m -d /home/$username -s /bin/bash $username
 mkdir -p /home/$username/.ssh && touch /home/$username/.ssh/authorized_keys && touch /home/$username/.ssh/id_ed25519 && touch /home/$username/.ssh/id_ed25519.pub
 chown -R $username:$username /home/$username/.ssh
